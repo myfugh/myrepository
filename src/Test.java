5aff1b7b2db4122e870a820a933bf14b650c03cb
myfugh <kalian_fgh@126.com>
2018-09-06 10:18:40 +0800

    增加换行符

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[] args){
-        System.out.print("hhhhhhhhhhhhhhhhhhhh" +
+        System.out.print("hhhhhhhhhhhhhhhhhhhh" +"\n"+
                 "jjjjjjjjjjjjjjjjjjjjj");
     }
 }
